834f3c8959292e227729092b89862b5c515b518f
Jayde Jeong <yljung@se.kaist.ac.kr>
2022-05-22 23:15:15 +0900

    Day 12 ~ 17 (excl. 55, 45, 5, 413)

---
+++ insanebear/LeetCode/1143_LCS.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[ 0 for _ in range(len(text1)+1)] for _ in range(len(text2)+1)]
+        
+        for i in range(1, len(text2)+1):
+            for j in range(1, len(text1)+1):
+                if text2[i-1] == text1[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+
+        return dp[-1][-1]
